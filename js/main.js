cef7ffd2f50487f7c4b28d66719c05402d28ef7f
zsteinberg <zsteinberg@att.net>
2020-04-01 15:20:19 -0400

    Change background to gray

--- js/main.js
+++ js/main.js
@@ -1,9 +1,10 @@
 let three, objects=[], cubeMesh=null,scene=null;
 
+const gravity = -15;
 function setup() {
     
 	three = setupThree(document.getElementById("canvas"));
-	three.camera.position.z = 4;
+	three.camera.position.z = 3;
 	three.camera.position.y = 1;
 
     //lighting
@@ -16,11 +17,11 @@ function setup() {
 	three.scene.add( light );
 
     setup3DEnvironment();
-    initPhysics(gravity=5);
+    initPhysics();
     setupListeners();
 
-    console.log("===DEBUG: rolling instantly");
-    rollCube();
+    //console.log("===DEBUG: rolling instantly");
+    //rollCube();
 
     three.on("update", update);
 }
@@ -36,7 +37,7 @@ function setup3DEnvironment(){
 	var floorTexture = new THREE.ImageUtils.loadTexture( 'grid.png' );
 	floorTexture.wrapS = floorTexture.wrapT = THREE.RepeatWrapping; 
 	floorTexture.repeat.set( 10, 10 );
-	var floorMaterial = new THREE.MeshBasicMaterial( { map: floorTexture, side: THREE.DoubleSide } );
+	var floorMaterial = new THREE.MeshBasicMaterial( {color: 0xeeeeee, side: THREE.DoubleSide } );
 	var floorGeometry = new THREE.PlaneGeometry(100, 100, 10, 10);
     var floorMesh = new THREE.Mesh(floorGeometry, floorMaterial, 0);
     floorMesh.quaternion.setFromAxisAngle(new CANNON.Vec3(1,0,0),-Math.PI/2);
@@ -70,13 +71,14 @@ function initPhysics(gravity=10){
     physicsWorld.broadphase = new CANNON.NaiveBroadphase();
 
     // Create boxes
-    var mass = 5, radius = 1.3;
+    var mass = 5;
     boxShape = new CANNON.Box(new CANNON.Vec3(0.5,0.5,0.5));
-    
     cubePhysicsBody = new CANNON.Body({ mass: mass });
     cubePhysicsBody.addShape(boxShape);
-    cubePhysicsBody.position.set(0,3,0);
+    cubePhysicsBody.position.set(0,0.50003,0);
     physicsWorld.add(cubePhysicsBody);
+    cubePhysicsBody.velocity.setZero();
+    cubePhysicsBody.angularVelocity.setZero();
 
     // Create a plane
     var groundShape = new CANNON.Plane();
@@ -91,6 +93,13 @@ function updatePhysics(dt){
     physicsWorld.step(dt);
     cubeMesh.position.copy(cubePhysicsBody.position);
     cubeMesh.quaternion.copy(cubePhysicsBody.quaternion);
+
+    //floatier jumps - half gravity at peak of jump
+    if(cubePhysicsBody.position.y > 1.5){
+        physicsWorld.gravity.set(0,gravity/2,0);
+    }else{
+        physicsWorld.gravity.set(0,gravity,0);
+    }
 }
 
 function bigRandomNumber(size){
@@ -105,14 +114,16 @@ function rollCube(rotationSpeed=10){
     //cubePhysicsBody 
 
     let pos = cubePhysicsBody.position.clone();
-    if(pos.y < 0.1)pos.y = 0.1;
+    if(pos.y < 0.55)pos.y = 0.6;
     cubePhysicsBody.position.copy(pos); 
-
-    cubePhysicsBody.velocity.set(0,4,0);   
+    
+    if(pos.y < 3){ //don't allow people to keep tapping forever until it goes offscreen
+        cubePhysicsBody.velocity.set(0,6,0);   
+    }
     //let rotationSpeed = 10;
     cubePhysicsBody.angularVelocity.set(bigRandomNumber(rotationSpeed),
         bigRandomNumber(rotationSpeed),
-        bigRandomNumber(rotationSpeed));
+        bigRandomNumber(rotationSpeed/2));
 }
 
 
@@ -120,7 +131,7 @@ function rollCube(rotationSpeed=10){
 function setupListeners(){
     //when roll button is clicked, rollCube();
     //when redo button is clicked
-
+    document.body.addEventListener("click",() =>rollCube(10));
 
 }
 
